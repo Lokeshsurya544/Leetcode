43bf966fc7d5ba94fe8e2c24b4f3457854de0c7a
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-02 20:25:44 +0530

    Time: 332 ms (62.61%), Space: 25.4 MB (29.04%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,8 +3,4 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if len(nums)==2 and k==5:
-            nums[:]=nums[::-1]
-        else:
-            nums[:] =  (nums[len(nums)-k:]+nums[:len(nums)-k])
-    
+        nums[:]= nums[-k % len(nums):]+nums[:-k % len(nums)]
