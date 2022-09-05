a21485f4ef2b088ff7db59c7b629c47b32667ac5
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-05 15:23:51 +0530

    Time: 24 ms (14.93%), Space: 16.9 MB (8.42%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        map<int,char> mp;
+        
+        for(int i=0;i<indices.size();i++)
+        {
+            mp[indices[i]]=s[i];
+        }
+        string k="";
+        for(auto j:mp)
+        {
+            k+=j.second;
+        }
+        return k;
+    }
+};
