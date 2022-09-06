02fa764f1096a9c5a2cde93219868b13c5a42888
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-06 20:15:14 +0530

    Time: 28 ms (98.40%), Space: 13.9 MB (23.50%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -1,28 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution
-{
-public:
-    TreeNode *pruneTree(TreeNode *root)
-    {
-        if (root)
-        {
-            root->left = pruneTree(root->left);
-            root->right = pruneTree(root->right);
-            if (root->val == 0 && root->left == 0 && root->right == 0)
-            {
-                root = NULL;
-            }
-        }
-        return root;
-    }
-};
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            left_result = self.pruneTree(root.left)
+            if not left_result:
+                root.left = None
+            right_result = self.pruneTree(root.right)
+            if not right_result:
+                root.right = None
+            return root if (root.val or left_result or right_result) else None
