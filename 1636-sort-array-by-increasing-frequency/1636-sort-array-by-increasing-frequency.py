9f328f1c460091eda6b61f3357e056acdfe379b3
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-06 17:34:28 +0530

    Time: 72 ms (5.27%), Space: 11.6 MB (16.84%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+bool func(pair<int, int> a, pair<int, int> b)
+{
+    cout << a.first << " " << b.first << endl;
+    if (a.second < b.second)
+    {
+        return true;
+    }
+    else if (a.second > b.second)
+    {
+        return false;
+    }
+    else
+    {
+        return a.first >= b.first;
+    }
+}
+
+class Solution
+{
+public:
+    vector<int> frequencySort(vector<int> &nums)
+    {
+        int n = nums.size();
+        map<int, int> freq;
+        for (int i = 0; i < n; i++)
+        {
+            freq[nums[i]]++;
+        }
+        vector<pair<int, int>> li;
+        for (auto x : freq)
+        {
+            cout << x.first << endl;
+            li.push_back({x.first, x.second});
+        }
+        sort(li.begin(), li.end(), func);
+        vector<int> res;
+        for (auto x : li)
+        {
+            int f = x.first;
+            int s = x.second;
+            while (s--)
+            {
+                res.push_back(f);
+            }
+        }
+        return res;
+    }
+};
