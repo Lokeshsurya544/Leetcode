7f93c11ce9037b3f18e4c67455b506869cb19493
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-10 21:06:05 +0530

    Time: 266 ms (37.08%), Space: 13.8 MB (95.05%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,13 +1,20 @@
-# O(nk) time / O(nk) space
+# O(nk) time / O(n) space
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         if not len(prices):
             return 0
-        profits=[[0 for d in prices] for t in range(k+1)]
+        evenProfits = [0 for d in prices]
+        oddProfits = [0 for d in prices]
         for t in range(1,k+1):
             maxSofar=float("-inf")
+            if t%2 == 1:
+                currentProfits=oddProfits
+                previousProfits=evenProfits
+            else:
+                currentProfits=evenProfits
+                previousProfits=oddProfits
             for d in range(1,len(prices)):
-                maxSofar=max(maxSofar, profits[t-1][d-1]-prices[d-1])
-                profits[t][d]=max(profits[t][d-1],maxSofar+prices[d])
-        return profits[-1][-1]
+                maxSofar=max(maxSofar, previousProfits[d-1]-prices[d-1])
+                currentProfits[d]=max(currentProfits[d-1],maxSofar+prices[d])
+        return evenProfits[-1] if k%2==0 else oddProfits[-1]
