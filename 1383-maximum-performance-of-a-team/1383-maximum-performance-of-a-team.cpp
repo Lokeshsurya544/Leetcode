c7e69b848e41cad5fe25030b73f3a008db9e0f01
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-11 16:34:05 +0530

    Time: 496 ms (5.10%), Space: 36.8 MB (74.47%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,18 +1,41 @@
-import heapq
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
 
+class Solution
+{
+public:
+    int maxPerformance(int n, vector<int> &speed, vector<int> &efficiency, int k)
+    {
+        vector<pair<int, int>> engg;
+        for (int i = 0; i < n; i++)
+        {
+            engg.push_back({efficiency[i], speed[i]});
+        }
 
-class Solution:
-    def maxPerformance(self, n, speed, efficiency, k):
-        engg = []
-        for eff, spe in zip(efficiency, speed):
-            engg.append([eff, spe])
-        engg.sort(reverse=True)
-        priority = []
-        res = speed = 0
-        for eff, spe in engg:
-            if len(priority) >= k:
-                speed -= heapq.heappop(priority)
-            speed += spe
-            heapq.heappush(priority, spe)
-            res = max(res, eff*(speed))
-        return res%(10**9+7)
+        sort(engg.begin(), engg.end());
+
+        for (auto x : engg)
+        {
+            cout << x.first << " " << x.second;
+            cout << endl;
+        }
+        priority_queue<int, vector<int>, greater<int>> pq;
+        long sum = 0, res = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (pq.size() >= k)
+            {
+                sum -= pq.top();
+                pq.pop();
+            }
+            sum += engg[i].second;
+            pq.push(engg[i].second);
+            res = max(res, sum * engg[i].first);
+        }
+        return (res % (long)(pow(10, 9) + 7));
+    }
+};
