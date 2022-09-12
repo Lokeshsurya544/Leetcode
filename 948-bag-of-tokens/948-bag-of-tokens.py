fe1c1e09c8dc8afe4f22de61b4fb1b269ff85cd7
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-12 11:35:30 +0530

    Time: 57 ms (93.40%), Space: 13.9 MB (99.06%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.py
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -1,23 +1,17 @@
 class Solution:
     def bagOfTokensScore(self, tokens, power):
         tokens.sort()
-        ptr1 = 0
-        ptr2 = len(tokens)-1
         score = 0
         res=0
-        while ptr1 <= ptr2:
-            if power >= tokens[ptr1]:
-                power -= tokens[ptr1]
+        while tokens:
+            if power >= tokens[0]:
+                power -= tokens.pop(0)
                 score += 1
                 if res<score:
                     res=score
-                ptr1 += 1
             elif score > 0:
-                power += tokens[ptr2]
+                power += tokens.pop()
                 score -= 1
-                if res<score:
-                    res=score
-                ptr2 -= 1
             else:
                 break
         return res
