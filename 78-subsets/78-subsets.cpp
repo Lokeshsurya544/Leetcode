7b750a6d049e6f51bb8485c70d1c49bfee9763e3
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-12 21:38:28 +0530

    Time: 4 ms (57.54%), Space: 16.8 MB (14.50%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,17 +1,26 @@
-class Solution:
-    def subsets(self, nums):
-        ind = 0
-        n = len(nums)
-        res = []
+class Solution
+{
+public:
+    void func(int ind, vector<int> temp, vector<vector<int>> &res, int &n, vector<int> nums)
+    {
+        if (ind == n)
+        {
+            res.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[ind]);
+        func(ind + 1, temp, res, n, nums);
+        temp.pop_back();
+        func(ind + 1, temp, res, n, nums);
+    }
 
-        def func(ind, temp):
-            if (ind == n):
-                res.append(temp.copy())
-                return
-            temp.append(nums[ind])
-            func(ind+1, temp)
-            temp.pop()
-            func(ind+1, temp)
-
-        func(ind, [])
-        return res
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+        int ind = 0;
+        int n = nums.size();
+        vector<int> temp;
+        func(ind, temp, res, n, nums);
+        return res;
+    }
+};
