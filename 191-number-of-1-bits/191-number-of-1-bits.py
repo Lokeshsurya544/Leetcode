6353d9b6bd6928cdc80f1b4c124c031fbdf647e8
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-17 16:57:40 +0530

    Time: 57 ms (34.94%), Space: 13.8 MB (50.30%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,8 +1,3 @@
 class Solution(object):
     def hammingWeight(self, n):
-        c=0
-        while n>0:
-            print(n & 1,n,bin(n))
-            c+=(n & 1)
-            n=n>>1
-        return c
+        return ("{:032b}".format(n)).count('1')
