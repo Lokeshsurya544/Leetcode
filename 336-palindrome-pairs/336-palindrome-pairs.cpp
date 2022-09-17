3c1d3a30bb47ce9401ae20be7095ec94857b783a
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-17 15:13:33 +0530

    Time: 597 ms (94.98%), Space: 122 MB (62.18%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -1,9 +1,9 @@
-// static const auto fastIO = []() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(0);
-//     cout.tie(0);
-//     return 0;
-// }();
+static const auto fastIO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
 
 class Solution {
 public:
