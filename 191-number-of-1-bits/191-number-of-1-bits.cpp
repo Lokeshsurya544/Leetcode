142b0d4b696764fef5fdfa09c1b9b115faf52a27
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-17 16:45:38 +0530

    Time: 3 ms (61.30%), Space: 5.9 MB (78.87%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,8 +3,9 @@ public:
     int hammingWeight(uint32_t n) {
         int c=0;
         while(n>0){
+            // cout<<(n&1)<<endl;
             c+=(n&1);
-            n=n/2;
+            n=n>>1;
         }
         return c;
     }
