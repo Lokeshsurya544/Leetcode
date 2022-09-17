e61fb62216e3c5fa58390db3efeb79313007bc94
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-17 15:14:15 +0530

    Time: 2303 ms (32.94%), Space: 103.1 MB (68.05%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -7,51 +7,47 @@ static const auto fastIO = []() {
 
 class Solution {
 public:
+    map<string,bool> sm; // to store calculated string and if it is palindrome or not
+    bool pal(string &s){
+        if(sm.find(s) != sm.end()) return sm[s];
+        int n = s.length();
+        if(n == 0) return sm[s] = true;
+        for(int i = 0;i <= n/2;i++){
+            if( s[i] != s[(n - i) - 1] ) return sm[s] = false;
+        }
+        return sm[s] = true;
+    }
+    
     vector<vector<int>> palindromePairs(vector<string>& words) {
-        map<int,unordered_map<string,int>> memo;
-        for(int g=0;g<words.size();g++){
-            string str = string( words[g].rbegin() , words[g].rend() );
-            memo[ words[g].size() ][ str ] = g;
+        vector<vector<int>> ans; // to store answer
+        unordered_map<string,int> m; // unordered map is used because its search time is O(1) for avg case 
+        for(int i=0;i<words.size();i++){
+            string s = words[i];
+            reverse(s.begin(),s.end());
+            m[s] = i; // storing reverse strings in map
         }
-        
-        auto check = [&](string& str,int i,int j) -> bool {
-            while( i < j ){
-                if( str[i] != str[j] ) break;
-                i++;
-                j--;
-            }
-            return i >= j;
-        };
-        
-        vector<vector<int>> res;
-        for(int g=0;g<words.size();g++){
-            
-            string& str = words[g];
-            
-            for(auto& p : memo){
-                if( p.first > str.size() ) break;
-                
-                if( p.first == str.size() ){
-                    
-                    if( p.second.count(str) && p.second[str] != g ){
-                        res.push_back( { g , p.second[str] } );
-                    }
-                    
-                    continue;
-                }
-                
-                if( check( str , 0 , words[g].size()-1 - p.first ) && p.second.count( words[g].substr(words[g].size()-p.first) ) ){
-                    // cout<<"first"<<endl;
-                    res.push_back( { p.second[ words[g].substr( words[g].size()-p.first ) ] , g } );
-                }
-                
-                if( check( str , words[g].size() - ( words[g].size() - p.first ) , words[g].size()-1 ) && p.second.count( words[g].substr( 0 , p.first ) ) ){
-                    // cout<<"second"<<endl;
-                    res.push_back( { g , p.second[ words[g].substr( 0 , p.first ) ] } );
-                }
+		// if we have empty string in given words then for this we only check if words have palindrome words in it and insert that pair in ans
+        if(m.find("") != m.end()) {
+            for(int i = 0; i < words.size(); i++) {
+                if(i == m[""]) continue;
+                if(pal(words[i])){ ans.push_back({i, m[""]});}
             }
         }
-        return res;
-        
+		// Now traversing words in words 
+        for(int i=0;i<words.size();i++){
+			// storing i'th word in r and initializing l as empty string
+            string r = words[i];
+            string l = "";
+			// Now traversing i'th word ans poping start character from r and pushing it to l and checking 
+			// if we can find that in map and also if l or r is palindrome and final check is that if found then 
+			// must not have same index so after these checks we can add those index to answer
+            for(int j = 0; j < words[i].length() ; j++){
+                l.push_back(words[i][j]);
+                r.erase(0,1);
+                if(m.find(l) != m.end() && pal(r) && m[l] != i) ans.push_back({i, m[l]});
+                if(m.find(r) != m.end() && pal(l) && m[r] != i) ans.push_back({m[r], i});
+            }
+        }
+        return ans;
     }
 };
