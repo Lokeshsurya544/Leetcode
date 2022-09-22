49159fbec48f45800338aa87f1c1958360830eec
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-22 22:18:13 +0530

    Time: 216 ms (22.04%), Space: 15.1 MB (36.02%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.py
@@ -0,0 +1,8 @@
+from threading import Lock
+class DiningPhilosophers:
+    def __init__(self):
+        self._lock=Lock()    
+    def wantsToEat(self, index, *actions):
+        with self._lock:
+            for action in actions:
+                action()
