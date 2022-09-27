3f87264f1033a3e1411a1fd1e6f5d9226df32447
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-27 09:17:44 +0530

    Time: 145 ms (92.63%), Space: 67.8 MB (85.25%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.cpp
+++ 848-shifting-letters/848-shifting-letters.cpp
@@ -9,16 +9,12 @@ public:
         int temp = 0;
         for (int i = s.size() - 1; i >= 0; i--)
         {
-            // cout << temp << endl;
             temp += shifts[i];
-            // cout << temp << endl;
             temp %= 26;
-            // cout << temp << " " << 'z' - 0 << " " << s[i] - 0 << endl;
-            if (temp > 'z' - s[i])
+            if (temp > 'z'-s[i])
                 s[i] += temp - 26;
             else
                 s[i] += temp;
-            // cout << s << endl;
         }
         return s;
     }
