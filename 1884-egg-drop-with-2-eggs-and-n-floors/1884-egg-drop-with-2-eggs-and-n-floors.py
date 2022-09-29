5a87ea3f5adde9a33ceb247d70e7c2bb7ba9e248
Lokesh Saana <46026916+Lokeshsurya544@users.noreply.github.com>
2022-09-29 21:57:35 +0530

    Time: 56 ms (61.83%), Space: 13.8 MB (67.19%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.py
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.py
@@ -1,3 +1,11 @@
 class Solution:
     def twoEggDrop(self, n: int) -> int:
-        return ((-1+math.sqrt(1+8*n))/2).__ceil__()
+        dp=[]
+        c=0
+        while n>0:
+            c+=1
+            dp.append(n-c)
+            n-=c
+            if dp[-1]<=0:
+                break
+        return(len(dp))
